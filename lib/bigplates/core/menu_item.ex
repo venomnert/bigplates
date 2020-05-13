05a1cc2deefc6e678d4c2d5ca678a454402d24df
venomnert <neerthigan@gmail.com>
2020-05-13 08:43:20 -0400

    Completed menu item

--- lib/bigplates/core/menu_item.ex
+++ lib/bigplates/core/menu_item.ex
@@ -1,17 +1,25 @@
 defmodule Bigplates.Core.MenuItem do
 
+  alias Bigplates.Core.PortionSize
+
   @price_types [:single, :per_person]
 
   defstruct name: nil,
             description: nil,
             price_type: nil,
             img: [],
-            portion_sizes: [],
+            portion_sizes: %{},
             published: true,
             variants: []
 
-  def create(fields) do
+  def new(fields) do
     struct!(__MODULE__, fields)
+  end
+
+  def add_portion_size(menu_item, fields) do
+    portion_size = PortionSize.new(fields)
+
+    menu_item |> Map.put(portion_size.name, portion_size)
   end
 
   def add_variant(menu_item, fields) do
