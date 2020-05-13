9699e4e01abc90ee046539b70e6140d7f07ca35e
venomnert <neerthigan@gmail.com>
2020-05-12 20:04:47 -0400

    Created utility module

---
+++ lib/bigplates/utility/utility.ex
@@ -0,0 +1,8 @@
+defmodule Bigplates.Utility do
+  def create_slug(string) do
+    string
+    |> String.downcase()
+    |> String.replace(~r/[^a-z0-9\s-]/, "")
+    |> String.replace(~r/(\s|-)+/, "-")
+  end
+end
