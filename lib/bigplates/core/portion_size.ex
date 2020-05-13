af85a243470c768f330c099cb710282769e505be
venomnert <neerthigan@gmail.com>
2020-05-13 08:43:35 -0400

    Updated portion sizes

--- lib/bigplates/core/portion_size.ex
+++ lib/bigplates/core/portion_size.ex
@@ -1,32 +1,25 @@
 defmodule Bigplates.Core.PortionSize do
 
-  """
-    * serving_range: [lower, higher]
 
+  """
     1. small: 1 - 8 people
     2. medium: 8 - 15 people
     3. large: 15 - 25 people
   """
 
-  defstruct price: nil,
-            sale_price: nil,
-            name: nil,
-            serving_range: [],
-            min_order: 1,
-            description: nil
+  defstruct ~w[name price sale_price min max min_order description]a
 
   def new(fields) do
     struct!(__MODULE__, fields)
+    |> generate_description("people")
   end
 
-  def generate_description(portion_size, entity) do
-    description = "#{portion_size.name}: #{get_min(portion_size)} - #{get_max(portion_size)} #{entity}"
+
+  defp generate_description(portion_size, entity) do
+    description = "#{portion_size.name}: #{portion_size.min} - #{portion_size.max} #{entity}"
 
     portion_size |> Map.put(:description, description)
   end
-
-  defp get_min(portion_size), do: portion_size.serving_range |> hd()
-  defp get_max(portion_size), do: portion_size.serving_range |> List.last()
 
 end
 
