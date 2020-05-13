e1cd5a00275c45893b7617c8ed34c3fd01970f24
venomnert <neerthigan@gmail.com>
2020-05-12 20:04:29 -0400

    Completed menu module

--- lib/bigplates/core/menu.ex
+++ lib/bigplates/core/menu.ex
@@ -1,14 +1,30 @@
 defmodule Bigplates.Core.Menu do
-  """
-    * meal_category: [:breakfast, :lunch, :dinner] - only one allowed
-  """
+  alias Bigplates.Utility
 
-  defstruct name: nil,
-            meal_category: nil,
-            published: true,
-            slug: nil
+  @meal_categories ~w[breakfast lunch dinner]a
 
-  def create(fields) do
-    struct!(__MODULE__, fields)
+  defstruct ~w[name meal_category slug]a
+
+  def new(fields) do
+    %__MODULE__{
+      name: fields.name,
+      meal_category: add_valid_category(fields),
+      slug: Utility.create_slug(fields.name)
+    }
+  end
+
+  def get_meal_categories(), do: @meal_categories
+
+  defp add_valid_category(fields) do
+    is_valid_category(fields.meal_category)
+    |> case do
+      true -> fields.meal_category
+      false -> Utility.create_slug(fields.name)
+    end
+  end
+
+  defp is_valid_category(category) do
+    @meal_categories
+    |> Enum.member?(category)
   end
 end
