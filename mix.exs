474157d7bc21ec54e31ecb1e275a4d96bf67ddb6
venomnert <neerthigan@gmail.com>
2020-05-17 10:44:56 -0400

    Added code quality tools

--- mix.exs
+++ mix.exs
@@ -7,7 +7,18 @@ defmodule Bigplates.MixProject do
       version: "0.1.0",
       elixir: "~> 1.10",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: deps(),
+
+      # Docs
+      name: "Bigplates",
+      source_url: "https://github.com/venomnert/bigplates",
+      homepage_url: "https://www.bigplates.ca",
+      docs: [
+        # The main page in the docs
+        main: "Bigplates",
+        logo: "path/to/logo.png",
+        extras: ["README.md"]
+      ]
     ]
   end
 
@@ -22,8 +33,9 @@ defmodule Bigplates.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      # {:dep_from_hexpm, "~> 0.3.0"},
-      # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
+      {:dialyxir, "~> 1.0", only: [:dev], runtime: false},
+      {:credo, "~> 1.4", only: [:dev, :test], runtime: false},
+      {:ex_doc, "~> 0.21", only: :dev, runtime: false}
     ]
   end
 end
