3ac353caa7e5ce55b2cf80746235c79bea8feafe
venomnert <neerthigan@gmail.com>
2020-05-17 16:06:35 -0400

    Completed combo item

--- lib/bigplates/core/combo_item.ex
+++ lib/bigplates/core/combo_item.ex
@@ -1,5 +1,7 @@
 defmodule Bigplates.Core.ComboItem do
 
+  alias Bigplates.Core.{MenuItem, PortionSize, Variant}
+
   defstruct name: nil,
             price: nil,
             sale_price: nil,
@@ -7,17 +9,29 @@ defmodule Bigplates.Core.ComboItem do
             cutlery: false,
             published: true,
             menu_items: [],
-            img: [],
-            portion_sizes: [],
-            variants: []
+            portion_sizes: %{},
+            variants: [],
+            img: []
 
   def new(fields) do
     struct!(__MODULE__, fields)
   end
 
-  def add_variant(combo_item, fields) do
+  def add_menu_item(combo_item, %MenuItem{} = menu_item) do
+    updated_menu = [menu_item] ++ combo_item.menu_items
+
+    combo_item |> Map.put(:menu_items, updated_menu)
   end
 
-  def add_menu_item(combo_item, fields) do
+  def add_portion_size(combo_item, fields) do
+    portion_size = PortionSize.new(fields)
+
+    combo_item |> Map.put(portion_size.name, portion_size)
+  end
+
+  def add_variant(combo_item, {variant_fields, variant_items}) do
+    updated_variants = [Variant.new({variant_fields, variant_items})] ++ combo_item.variants
+
+    combo_item |> Map.put(:variants, updated_variants)
   end
 end
