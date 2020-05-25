d2f47cf696001b4eb8f714e59ec93b2c5c106021
venomnert <neerthigan@gmail.com>
2020-05-25 18:39:53 -0400

    refactored testing

--- test/restaurant_test.exs
+++ test/restaurant_test.exs
@@ -2,8 +2,8 @@ defmodule RestaurantTest do
   use ExUnit.Case
   use BigplatesBuilder
 
-  describe "adding & removing cuisine type to restaurant" do
-    setup [:restaurant]
+  describe "adding & updating cuisine type to restaurant" do
+    setup [:restaurant_no_cuisine_type]
 
     test "Add cuisine type to restaurant", %{restaurant: restaurant} do
       new_cuisine = cuisinine_fields() |> CuisineType.new()
@@ -38,24 +38,63 @@ defmodule RestaurantTest do
     end
 
     # Since this throws an error, should this be handled by Boundry layer?
-    test "Invalid update cuisine type call", %{restaurant: restaurant} do
-      new_cuisine_1 = cuisinine_fields() |> CuisineType.new()
+    # Revisit this
+    # test "Invalid update cuisine type call", %{restaurant: restaurant} do
+    #   new_cuisine_1 = cuisinine_fields() |> CuisineType.new()
 
-      new_cuisine_2 =
-        cuisinine_fields(%{
-          sri_lankan: true,
-          english: true
-        })
+    #   new_cuisine_2 =
+    #     cuisinine_fields(%{
+    #       sri_lankan: true,
+    #       english: true
+    #     })
 
-      restaurant
-      |> Restaurant.add_cuisine(new_cuisine_1)
-      |> assert_cuisine_type(new_cuisine_1)
-      |> Restaurant.update_cuisine(new_cuisine_2)
+    #   restaurant
+    #   |> Restaurant.add_cuisine(new_cuisine_1)
+    #   |> assert_cuisine_type(new_cuisine_1)
+    #   |> Restaurant.update_cuisine(new_cuisine_2)
+    #   |> assert_cuisine_types_error()
+    # end
+  end
+
+  describe "adding & updating requirements" do
+    setup [:restaurant_no_requirement_type]
+
+    test "Add requirement to restaurant", %{restaurant: restaurant} do
+      
+      restaurant_cuisine_types =
+        restaurant
+        |> Restaurant.add_cuisine(new_cuisine)
+        |> Map.get(:cuisine_types)
+
+      assert restaurant_cuisine_types == new_cuisine
     end
   end
 
-  defp restaurant(context) do
-    restaurant = create_restaurant() |> Restaurant.new()
+  defp restaurant_no_cuisine_type(context) do
+    restaurant =
+      create_restaurant(%{
+        requirements: %{
+          minimum_time: 24,
+          minimum_order: 300,
+          delivery_fee: %{
+            fee: 20,
+            waive_after: 1000
+          }
+        }
+      })
+      |> Restaurant.new()
+
+    {:ok, Map.put(context, :restaurant, restaurant)}
+  end
+
+  defp restaurant_no_requirement_type(context) do
+    new_cuisine = cuisinine_fields() |> CuisineType.new()
+
+    restaurant =
+      create_restaurant()
+      |> Restaurant.new()
+      |> Restaurant.add_cuisine(new_cuisine)
+
     {:ok, Map.put(context, :restaurant, restaurant)}
   end
 
