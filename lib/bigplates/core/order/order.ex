58606d51d7e91bfe825599802f23015fb4d7f64e
venomnert <neerthigan@gmail.com>
2020-10-10 12:30:21 -0400

    Refactored CRUD for order items

--- lib/bigplates/core/order/order.ex
+++ lib/bigplates/core/order/order.ex
@@ -29,33 +29,38 @@ defmodule Bigplates.Core.Order do
             delivery_time_window_minutes: [],
             transactions: [],
             item_total: nil,
-            total_items: []
+            total_items: %{}
 
   def new(fields) do
     struct!(__MODULE__, fields)
   end
 
-  def add_item(order, menu_item) do
-    {_, updated_order} =
+  def add_order_item(order, order_item) do
+    new_total_items =
       order
-      |> process_order_action(menu_item)
-      |> Map.get_and_update(:total_items, &add_to_total_item(&1, menu_item))
-
-    updated_order
-  end
-
-  def update_item(order, menu_item) do
-  end
-
-  def delete_item(order, menu_item) do
-    updated_total_items =
-      order
-      |> process_order_action(menu_item)
       |> Map.get(:total_items)
-      |> Enum.reject(&delete_from_total_item(&1, menu_item))
+      |> Map.update(order_item, 1, &(&1+ 1))
+
+    Map.put(order, :total_items, new_total_items)
+  end
+
+  def reduce_order_item(order, order_item) do
+    {_old_total_items, new_order_items} =
+      order
+      |> Map.get(:total_items)
+      |> Map.get_and_update(order_item, &check_and_reduce(&1))
 
     order
-    |> Map.put(:total_items, updated_total_items)
+    |> Map.put(:total_items, new_order_items)
+  end
+
+  def remove_order_item(order, order_item) do
+    new_total_items =
+      order
+      |> Map.get(:total_items)
+      |> Map.delete(order_item)
+
+    Map.put(order, :total_items, new_total_items)
   end
 
   def process_order_action(order, menu_item) do
@@ -98,10 +103,8 @@ defmodule Bigplates.Core.Order do
   def calculate_sub_total(%{valid: true} = order), do: order
   def calculate_sub_total(%{valid: false} = order), do: order
 
-  defp add_to_total_item(menu_items, new_menu_item),
-    do: {menu_items, [new_menu_item | menu_items]}
+  defp check_and_reduce(qty) when qty == 0, do: :pop
+  defp check_and_reduce(qty) when qty > 0, do: {qty, qty - 1}
 
-  defp delete_from_total_item(menu_item, old_menu_item),
-    do: menu_item == old_menu_item
 
 end
--- test/order_test.exs
+++ test/order_test.exs
@@ -5,27 +5,79 @@ defmodule OrderTest do
   @default_order_requirement %{minimum_time: 0, minimum_order: 0}
   @default_delivery_requirement %{fee: 0, waive_after: 0}
 
-  describe "CRUD simple menu items" do
-    test "Add simple menu items" do
+  describe "CRUD simple order items" do
+    test "Add simple order items" do
       order_1 = order_fields()
+
+      restaurant_1 = restaurant_fields() |> Restaurant.new()
+      restaurant_2 = restaurant_fields(%{name: "gabbys"}) |> Restaurant.new()
+
       menu_item_1 = menu_items_fields() |> MenuItem.new()
-      menu_item_2 = menu_items_fields(%{
-                      name: "pancakes",
-                      categories: [:lunch, :dinner],
-                      description: "Fluffy, delicious, crunchy pancakes with Canadian maple syrup.",
-                    })
-                    |> MenuItem.new()
+      menu_item_2 =
+        menu_items_fields(%{
+          name: "pancakes",
+          categories: [:lunch, :dinner],
+          description: "Fluffy, delicious, crunchy pancakes with Canadian maple syrup."
+        })
+        |> MenuItem.new()
+
+      order_item_1 = {restaurant_1, menu_item_1}
+      order_item_2 = {restaurant_1, menu_item_2}
+      order_item_3 = {restaurant_2, menu_item_2}
 
       order_1
       |> Order.new()
-      |> Order.add_item(menu_item_1)
-      |> assert_menu_item(menu_item_1)
-      |> Order.add_item(menu_item_2)
-      |> assert_menu_item(menu_item_2)
+      |> Order.add_order_item(order_item_1)
+      |> assert_order_item_add(order_item_1, 1)
+      |> Order.add_order_item(order_item_1)
+      |> assert_order_item_add(order_item_1, 2)
+      |> Order.add_order_item(order_item_2)
+      |> assert_order_item_add(order_item_2, 1)
+      |> Order.add_order_item(order_item_3)
+      |> Order.add_order_item(order_item_3)
+      |> assert_order_item_add(order_item_3, 2)
     end
 
-    test "Delete simple menu items" do
+    test "Reduce simple order items" do
       order_1 = order_fields()
+
+      restaurant_1 = restaurant_fields() |> Restaurant.new()
+      restaurant_2 = restaurant_fields(%{name: "gabbys"}) |> Restaurant.new()
+
+      menu_item_1 = menu_items_fields() |> MenuItem.new()
+      menu_item_2 =
+        menu_items_fields(%{
+          name: "pancakes",
+          categories: [:lunch, :dinner],
+          description: "Fluffy, delicious, crunchy pancakes with Canadian maple syrup."
+        })
+        |> MenuItem.new()
+
+      order_item_1 = {restaurant_1, menu_item_1}
+      order_item_2 = {restaurant_1, menu_item_2}
+      order_item_3 = {restaurant_2, menu_item_2}
+
+      order_1
+      |> Order.new()
+      |> Order.add_order_item(order_item_1)
+      |> Order.add_order_item(order_item_1)
+      |> Order.add_order_item(order_item_1)
+      |> Order.add_order_item(order_item_2)
+      |> Order.add_order_item(order_item_3)
+      |> Order.add_order_item(order_item_3)
+      |> assert_order_item_add(order_item_3, 2)
+      |> Order.reduce_order_item(order_item_1)
+      |> assert_order_item_add(order_item_1, 2)
+      |> Order.reduce_order_item(order_item_2)
+      |> assert_order_item_removal(order_item_1)
+    end
+
+    test "Remove simple order item" do
+      order_1 = order_fields()
+
+      restaurant_1 = restaurant_fields() |> Restaurant.new()
+      restaurant_2 = restaurant_fields(%{name: "gabbys"}) |> Restaurant.new()
+
       menu_item_1 = menu_items_fields() |> MenuItem.new()
       menu_item_2 = menu_items_fields(%{
                       name: "pancakes",
@@ -34,15 +86,23 @@ defmodule OrderTest do
                     })
                     |> MenuItem.new()
 
+      order_item_1 = {restaurant_1, menu_item_1}
+      order_item_2 = {restaurant_1, menu_item_2}
+
       order_1
       |> Order.new()
-      |> Order.add_item(menu_item_1)
-      |> assert_menu_item(menu_item_1)
-      |> Order.add_item(menu_item_2)
-      |> assert_menu_item(menu_item_2)
-      |> Order.delete_item(menu_item_1)
-      |> assert_menu_item_removal(menu_item_1)
-      |> assert_menu_item(menu_item_2)
+      |> Order.add_order_item(order_item_1)
+      |> Order.add_order_item(order_item_1)
+      |> Order.add_order_item(order_item_1)
+      |> Order.add_order_item(order_item_2)
+      |> Order.add_order_item(order_item_2)
+      |> assert_order_item_add(order_item_2, 2)
+      |> Order.remove_order_item(order_item_1)
+      |> assert_order_item_removal(order_item_1)
+      |> assert_order_item_add(order_item_2, 2)
+      |> Order.remove_order_item(order_item_2)
+      |> assert_order_item_removal(order_item_2)
+
     end
   end
 
@@ -52,15 +112,13 @@ defmodule OrderTest do
     {:ok, Map.put(context, :order, order)}
   end
 
-
-  defp assert_menu_item(order, menu_item) do
-    assert Enum.member?(order.total_items, menu_item) == true
+  defp assert_order_item_add(order, order_item, qty) do
+    assert Map.get(order.total_items, order_item) == qty
     order
   end
 
-  defp assert_menu_item_removal(order, menu_item) do
-    assert Enum.member?(order.total_items, menu_item) == false
+  defp assert_order_item_removal(order, order_item) do
+    assert Enum.member?(order.total_items, order_item) == false
     order
   end
-
 end
